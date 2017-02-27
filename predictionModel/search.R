ef709766d6b1abf154a04e0dcbb1424bb240d262
Breenbo <berrehuelbruno@gmail.com>
2017-02-28 00:01:50 +0100

    Changes to be committed: 	modified:   search.R

--- predictionModel/search.R
+++ predictionModel/search.R
@@ -8,10 +8,17 @@ trigram <- fread("trigrams.csv", stringsAsFactors=F)
 quadgram <- fread("quadgrams.csv", stringsAsFactors=F)
 pentagram <- fread("pentagrams.csv", stringsAsFactors=F)
 
-set.seed(2704)
-triSample <- sample_n(trigram, size = 10000)
-triSample %>% count(ngram, sort=T)
+set.seed(2706)
+triSample <- sample_n(trigram, size = 100000)
+triSample <- triSample %>% count(ngram, sort=T)
+triSample %>% filter(grepl("^is been", ngram))
 
-quadsample <- sample_n(quadgram, size = 10000)
-quadsample %>% count(ngram, sort=T)
+quadsample <- sample_n(quadgram, size = 100000)
+quadsample <- quadsample %>% count(ngram, sort=T)
+quadsample %>% filter(grepl("^it is been", ngram))
+
+
+library(tidyr)
+quadsample <- quadsample %>% separate(col = ngram, into = c("word1", "word2", "word3", "word4"), sep = " ")
+quadsample %>% unite(col = gram, 1:3, sep=" ") %>% filter(gram=="at the same")
 
